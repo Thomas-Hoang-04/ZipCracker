32e3cad836c305642bdf39a64513c0b734cc76d7
Minh Hai Hoang <122735724+Thomas-Hoang-04@users.noreply.github.com>
2024-12-03 22:04:21 +0700

    chore: move resource directory

--- src/main/kotlin/com/thomas/zip/Main.kt
+++ src/main/kotlin/com/thomas/zip/Main.kt
@@ -5,8 +5,8 @@ import java.io.File
 fun readFile(filename: String) = File(filename).inputStream().readBytes()
 
 fun main() {
-    val filename = System.getProperty("user.dir") + "/src/main/resources/hello.zip"
-    val fileE  = System.getProperty("user.dir") + "/src/main/resources/hello-e.zip"
+    val filename = System.getProperty("user.dir") + "/src/resources/hello.zip"
+    val fileE  = System.getProperty("user.dir") + "/src/resources/hello-e.zip"
     val res = readFile(filename).joinToString(" ") {
         byte -> "0x%02x".format(byte)
     }

4eda47a5ceb19f6ab71c9063a13a30ff67cbb083
Minh Hai Hoang <122735724+Thomas-Hoang-04@users.noreply.github.com>
2024-12-03 21:59:28 +0700

    init project

---
+++ build.gradle.kts
@@ -0,0 +1,24 @@
+plugins {
+    kotlin("jvm") version "1.9.25"
+}
+
+group = "com.thomas"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(21)
+    compilerOptions {
+        freeCompilerArgs.addAll("-Xjsr305=strict")
+    }
+}

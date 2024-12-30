83d7ea6a152f66b2abbdd3c189a2907699e6d9a9
Minh Hai Hoang <122735724+Thomas-Hoang-04@users.noreply.github.com>
2024-12-31 01:18:06 +0700

    chore: clean utility files

--- composeApp/src/desktopMain/kotlin/com/thomas/zipcracker/Platform.kt
+++
@@ -1,7 +0,0 @@
-package com.thomas.zipcracker
-
-class JVMPlatform {
-    val name: String = "Java ${System.getProperty("java.version")}"
-}
-
-fun getPlatform(): JVMPlatform = JVMPlatform()
--- composeApp/src/desktopMain/kotlin/com/thomas/zipcracker/utility/DecompressTest.kt
+++
@@ -1,9 +0,0 @@
-package com.thomas.zipcracker.utility
-
-import net.lingala.zip4j.ZipFile
-
-fun main() {
-    val file = ZipFile(System.getProperty("user.dir") + "/src/main/resources/test_3_pk.zip")
-    file.setPassword("engineering".toCharArray())
-    file.extractAll(System.getProperty("user.dir") + "/output")
-}

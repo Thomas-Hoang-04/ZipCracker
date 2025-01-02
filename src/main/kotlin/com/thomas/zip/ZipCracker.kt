a87952e5acf3ba0353d50a0032d6df1f76e38a1c
Minh Hai Hoang <122735724+Thomas-Hoang-04@users.noreply.github.com>
2025-01-03 00:01:14 +0700

    chore: update test code

--- src/main/kotlin/com/thomas/zip/ZipCracker.kt
+++ src/main/kotlin/com/thomas/zip/ZipCracker.kt
@@ -1,11 +1,22 @@
 package com.thomas.zip
 
-
-
+import java.time.LocalDate
+import java.time.ZoneId
+import java.time.ZonedDateTime
+import java.time.format.DateTimeFormatter
+import kotlin.time.Duration.Companion.seconds
 
 
 fun main() {
-    val filename = System.`in`.bufferedReader().readLine()
-    println(filename)
+//    val filename = System.getProperty("user.home")
+//    println(filename)
+//    val timeZone = ZoneId.systemDefault()
+//    println("System TimeZone ID: " + timeZone.id)
+    val current = ZonedDateTime.now(ZoneId.systemDefault())
+    val date = LocalDate.ofInstant(current.toInstant(), ZoneId.systemDefault())
+    val time = current.toLocalTime().toSecondOfDay()
+    val storeDate = DateTimeFormatter.ofPattern("dd/MM/YYYY").format(date)
+    val timestamp = "${storeDate}, ${time.seconds}"
+    println(timestamp)
 }
 

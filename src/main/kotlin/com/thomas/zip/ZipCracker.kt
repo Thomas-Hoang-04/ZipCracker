be8a7f9f32b1235e340abbf324be322c526111ad
Minh Hai Hoang <122735724+Thomas-Hoang-04@users.noreply.github.com>
2025-01-04 00:05:56 +0700

    feat: testing for large file processing

--- src/main/kotlin/com/thomas/zip/ZipCracker.kt
+++ src/main/kotlin/com/thomas/zip/ZipCracker.kt
@@ -1,7 +1,9 @@
 package com.thomas.zip
 
 import com.thomas.zip.utility.*
+import net.lingala.zip4j.io.inputstream.ZipInputStream
 import java.io.File
+import java.io.FileInputStream
 import java.time.LocalDate
 import java.time.ZoneId
 import java.time.ZonedDateTime
@@ -28,11 +30,27 @@ fun main() {
 //    val timestamp = "${storeDate}, ${time.seconds}"
 //    println(timestamp)
 
-    val filename = System.getProperty("user.dir") + "/resources/test.zip"
-    val file = readFile(filename).toRawString()
-    val extracted = extractZip(file)[5]
-    println(isDirectory(extracted))
-    println(extracted.getPrintByte())
-    println(0x2f.toChar() == File.separatorChar)
+    println(System.getProperty("user.home")+File.separatorChar+"Desktop")
+//    val filename = System.getProperty("user.dir") + "/resources/hello_10_nest.zip"
+//    val file_2 = System.getProperty("user.dir") + "/resources/test_2_pk.zip"
+//    processZipEntries(filename)
+//    processZipEntries(file_2)
+//    val file = readFile(filename).toRawString()
+//    val extracted = extractZip(file)[0]
+//    println(isDirectory(extracted))
+//    println(extracted.getPrintByte())
+//    println(0x2f.toChar() == File.separatorChar)
+}
+
+fun processZipEntries(path: String) {
+    FileInputStream(path).use { fis ->
+        ZipInputStream(fis).use { zip ->
+            var entry = zip.nextEntry
+            while (entry != null) {
+                println(entry.isEncrypted)
+                entry = zip.nextEntry
+            }
+        }
+    }
 }
 
--- src/main/kotlin/com/thomas/zip/utility/DecompressTest.kt
+++ src/main/kotlin/com/thomas/zip/utility/DecompressTest.kt
@@ -1,9 +1,36 @@
 package com.thomas.zip.utility
 
 import net.lingala.zip4j.ZipFile
+import net.lingala.zip4j.io.inputstream.ZipInputStream
+import java.io.File
+import java.io.FileInputStream
 
 fun main() {
-    val file = ZipFile(System.getProperty("user.dir") + "/src/main/resources/test_3_pk.zip")
-    file.setPassword("engineering".toCharArray())
-    file.extractAll(System.getProperty("user.dir") + "/output")
+//    val file = ZipFile("D:\\Bài tập lớn.zip")
+//    file.setPassword("hello".toCharArray())
+//    file.extractAll(System.getProperty("user.dir") + "/output")
+    var encrypted = false
+    FileInputStream("D:\\Java\\zip\\resources\\Classifieds.zip").use { fis ->
+        ZipInputStream(fis).use { zis ->
+            zis.setPassword("hana".toCharArray())
+            try {
+                var entry = zis.nextEntry
+                println(entry)
+                while (entry != null) {
+                    if (entry.isDirectory) { entry = zis.nextEntry; continue }
+                    if (entry.isEncrypted) {
+                        encrypted = true
+                        break
+                    }
+                    entry = zis.nextEntry
+                }
+            } catch (e: Exception) {
+                if (e.message?.contains("Wrong Password") == true) {
+                    encrypted = true
+                }
+                println(e.stackTraceToString())
+            }
+        }
+    }
+    println(encrypted)
 }
